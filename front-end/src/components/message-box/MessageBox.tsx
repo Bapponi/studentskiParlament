0190a8a9d0ad4b4337e3b78bbdb3d4b808eba341
Bapponi <aleksandarbubalo99@gmail.com>
2024-08-13 19:37:44 +0200

    dodate ikonice na MessageBoc komponentu

--- front-end/src/components/message-box/MessageBox.tsx
+++ front-end/src/components/message-box/MessageBox.tsx
@@ -15,15 +15,24 @@ interface MessageBoxProps {
 const MessageBox: React.FC<MessageBoxProps> = ({text, infoType}) => {
 
 return (
-    <div className='message-box__container'>
+    <div>
       {infoType == MessageBoxTypes.Info && (
-        <h2>{text}</h2>
+        <div className='message-box__container'>
+          <img src="../checked.png" alt="checked" className='message-box__icon'/>
+          <h2>{text}</h2>
+        </div>
       )}
       {infoType == MessageBoxTypes.Error && (
-        <h2 style={{color: "var(--primary-color)"}}>{text}</h2>
+        <div className='message-box__container'>
+          <img src="../warning.png" alt="warning" className='message-box__icon'/>
+          <h2 style={{color: "var(--primary-color)"}}>{text}</h2>
+        </div>
       )}
       {infoType == MessageBoxTypes.Loading && (
-        <h2>{text}</h2>
+        <div className='message-box__container'>
+          <img src="../loading.gif" alt="loading" className='message-box__icon'/>
+          <h2>{text}</h2>
+        </div>
       )}
     </div>
   );
